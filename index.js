2854d771b61d2c6fc9f93d6e213b69b0b99039b4
Crystal <crystal_wright@yahoo.com>
2018-01-13 22:22:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
 function sayHiToGrandma (string) {
     
     if (string === string.toLowerCase()) {
-      return 
-    }
+      return `I can't hear you!`;
+    } else if 
     
 }
