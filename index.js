a4485c3027658626d788aa75fa99a953100e5c84
Crystal <crystal_wright@yahoo.com>
2018-01-13 22:21:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma (string) {
     
-    if (string === string.toLowerCase())
-      
+    if (string === string.toLowerCase()) {
+      return 
+    }
     
 }
